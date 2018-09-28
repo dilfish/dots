73a8433ed5140cbc46d3494ea94320bf5a086abe
sean <sean@remote>
2018-09-28 15:02:47 +0800

    add a real domain for test

--- dots_test.go
+++ dots_test.go
@@ -3,6 +3,7 @@ package dots
 import (
 	"github.com/miekg/dns"
 	"testing"
+    "fmt"
 )
 
 func TestMakeClient(t *testing.T) {
@@ -19,12 +20,27 @@ func sendDns() error {
 	m.Question = make([]dns.Question, 1)
 	m.Question[0] = dns.Question{"baidu.com.", dns.TypeA, dns.ClassINET}
 	c := new(dns.Client)
-	_, _, err := c.Exchange(m, "127.0.0.1:853")
-	return err
+    c.Net = "tcp-tls"
+    conn, err := c.Dial("local.xn--oht.com:853")
+    if err != nil {
+        println("dial", err)
+        return err
+    }
+    err = conn.WriteMsg(m)
+    if err != nil {
+        println("write", err)
+        return err
+    }
+    bt := make([]byte, 5400)
+    n, err := conn.Read(bt)
+    if err != nil {
+        fmt.Println("read", n, err, bt)
+    }
+    return err
 }
 
 func TestGetLisener(t *testing.T) {
-	ls, err := GetListener("testdata/certs/client.pem", "testdata/certs/client.key")
+	ls, err := GetListener("testdata/certs/full.pem", "testdata/certs/priv.pem")
 	if err != nil {
 		t.Error("get ls err", err)
 	}
